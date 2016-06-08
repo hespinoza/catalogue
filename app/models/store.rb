bf7de387172c8aad1032dc4802ad1bca33d2d39a
Héctor Espinoza <Hector@MacBook-Pro-de-Hector.local>
2016-06-07 21:38:00 -0300

    Modify edit stores view

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,7 +2,7 @@ class Store < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: :slugged
 
-	validates :name, presence: true
+	validates :name, :presence => { :message => " no puede estar vacío" }
 	validates :category, presence: true
 	validates :description, presence: true
   belongs_to :user
